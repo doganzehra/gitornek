a7ed743dd20142640707b3fd8ff9c987005e5a00
doganzehra <doganzehra20@gmail.com>
2022-09-26 14:39:59 +0300

    App.java dosyası düzeltildi.

--- src/main/java/com/bilgeadam/gitornek/App.java
+++ src/main/java/com/bilgeadam/gitornek/App.java
@@ -1,11 +1,8 @@
 package com.bilgeadam.gitornek;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello first commit" );
-        
-        System.out.println( "Hello first commit 2" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello first commit");
+
+	}
 }
