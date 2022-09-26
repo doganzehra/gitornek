155dc0bcad57df38f7525417c11ccdea77f7f5e5
doganzehra <doganzehra20@gmail.com>
2022-09-26 15:29:53 +0300

    test için ayrı bir branch oluşturuldu.

--- src/main/java/com/bilgeadam/gitornek/App.java
+++ src/main/java/com/bilgeadam/gitornek/App.java
@@ -2,9 +2,11 @@ package com.bilgeadam.gitornek;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello first commit");
-
-		System.out.println("Hello first commit 3");
+		Person person = new Person();
 	}
+
+}
+
+class Person {
 
 }
